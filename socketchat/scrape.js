dc2f9a68f30fcb4e95f1ffb6d2f05624685d4db6
Selynna Sun <ssun098@gmail.com>
2016-01-23 04:23:18 -0500

    Add functionality to scraping - title comment date

--- socketchat/scrape.js
+++ socketchat/scrape.js
@@ -7,12 +7,21 @@ request('https://api.everyblock.com/content/philly/locations/19104/timeline/?tok
     var results = body.results;
     var step;
     for (step = 0; step < 25; step++) {
-      // var lat = results[1].location_coordinates[0];
-      // console.log(lat)
+      var title = results[step].title;
+      console.log("title: " + title);
+      if (results[step].attributes.comment != undefined) {
+        var comment = results[step].attributes.comment;
+      } else {
+        var comment = results[step].attributes.excerpt;
+      }
+      console.log("comment: " + comment);
       var latitude = results[step].location_coordinates[0].latitude;
       console.log("latitude: " + latitude);
       var longitude = results[step].location_coordinates[0].longitude;
       console.log("longitude: " + longitude);
+      var date = results[step].pub_date;
+      console.log("date: " + date);
+
    }
   }
 
